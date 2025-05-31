316526bdd2ab86ce3f74623d23ebbcefb3ccd322
Thomas Blein <thomas.blein@ips2.universite-paris-saclay.fr>
2025-05-31 17:06:50 +0200

    feat: next image timing on main page

--- app/experimentlist/__init__.py
+++ app/experimentlist/__init__.py
@@ -2,6 +2,7 @@
 The main views of the application
 """
 from app.experimentlist.models import ExperimentList
+from app.options.schedulerstatus import SchedulerStatus
 from config import Config
 from flask import Blueprint, abort, render_template
 from jinja2 import TemplateNotFound
@@ -14,6 +15,8 @@ experiment_page = Blueprint('experiment_page', __name__,
                             template_folder='templates',
                             static_folder='static')
 
+schedulerstatus = SchedulerStatus()
+
 
 @main_page.route('/index')
 @main_page.route('/')
@@ -22,6 +25,9 @@ def experiment_status():
     Return a summary of all the experiment of the module
     """
     exps = ExperimentList(directory=Config.WORKING_DIR)
+    for exp in exps.exps:
+        if exp.expid in schedulerstatus.jobs_info:
+            exp.next_run_time = schedulerstatus.jobs_info[exp.expid]["next_run_time"]
     devicetime = '{0:%Y-%m-%d %H:%M:%S}'.format(datetime.now())
     try:
         return render_template('index.html', exps=exps, now=devicetime,
